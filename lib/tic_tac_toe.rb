81e6025a1de7e775854b9af571ee13d2c76213f6
Bobbi York <york1670@gmail.com>
2018-09-18 02:18:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,8 +27,8 @@ def input_to_index(user_input)
 end
 
 def move(index, token = "X")
-  @index = @board[@input_to_index]
-  @token = @current_player
+  @board[index] = token
+  @token = current_player
 end
 
 def position_taken?(index)
